920371be5f97257ea751b04c921c24eb04ad5148
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-10-20 18:17:21 +0700

    feat(scss): add erk-m-... custom styles with scss (included snippet code for suggestion)

---
+++ app/scss/basic/page.tsx
@@ -0,0 +1,3 @@
+export default function BasicScssPage(): JSX.Element {
+  return <div className="erk-m-128">Basic SCSS Page</div>;
+}
--- assets/Svgs/Frame/index.tsx
+++ assets/Svgs/Frame/index.tsx
@@ -1,18 +1,18 @@
-import styles from '@/styles/Svg.module.css';
+import styles from "@/styles/css/Svg.module.css";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-import { Svg } from '@/interfaces';
+import { Svg } from "@/interfaces";
 
 export default function Frame({
   children,
   size = 24,
-  linkTo = '#',
+  linkTo = "#",
   padding = 0,
-  className = '',
+  className = "",
   divRef = null,
   isActive = false,
-  onClick,
+  onClick
 }: Svg.ISvgProps): JSX.Element {
   const router = useRouter();
   const handleOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -25,10 +25,10 @@ export default function Frame({
       style={{
         width: `${size + 2 * padding}px`,
         padding: `${padding}px`,
-        display: 'flex',
-        cursor: isActive ? 'pointer' : 'default',
+        display: "flex",
+        cursor: isActive ? "pointer" : "default"
       }}
-      className={`${isActive ? styles.active : ''} ${className}`}
+      className={`${isActive ? styles.active : ""} ${className}`}
       onClick={handleOnClick}
     >
       {children}
